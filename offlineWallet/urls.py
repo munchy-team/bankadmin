d693691eb4e316c22de259fd2a39f0732d8ada0a
coding986532 <97757073+coding986532@users.noreply.github.com>
2022-06-29 11:53:23 -0700

    couple of updates

--- offlineWallet/urls.py
+++ offlineWallet/urls.py
@@ -35,3 +35,10 @@ urlpatterns = [
     path('', home1),
     path('admin/', admintrap)
 ]
+
+if settings.DEBUG==False:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+   #urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_URL)
+   #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
