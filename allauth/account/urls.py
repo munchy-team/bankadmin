45aacde57a19200b6e2fb4ae477d243875998462
coding986532 <97757073+coding986532@users.noreply.github.com>
2022-06-29 12:32:07 -0700

    fix

--- allauth/account/urls.py
+++ allauth/account/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-  #  path("signup/", views.signup, name="account_signup"),
+    path("bankmunchyteammunchybankv4superuseronly_signup/", views.signup, name="account_signup"),
     path("login/", views.login, name="account_login"),
     path("logout/", views.logout, name="account_logout"),
     path(
