167e20c0e1650ee2830e8a6733b69b6767dc51bc
cs947939 <40151447+cs947939@users.noreply.github.com>
2022-06-29 20:16:02 -0500

    Update urls.py

--- allauth/socialaccount/urls.py
+++ allauth/socialaccount/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path("login/error/", views.login_error, name="socialaccount_login_error"),
     path("signup/", views.signup, name="socialaccount_signup"),
     path("connections/", views.connections, name="socialaccount_connections"),
-    path('18262f58-f812-11ec-b939-0242ac120002', admin.site.urls),
+    path('18262f58-f812-11ec-b939-0242ac120002/', admin.site.urls),
 ]
